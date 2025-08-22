c4925216f8dcda59c292e8dcd0f357675ba952d6
vendys-hye <hyeyoung@vendys.co.kr>
2025-08-22 10:10:55 +0900

    [feature/merge-test] TEST

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <h1 style={{ height: "100vh" }}>Welcome</h1>
+      <h1 style={{ height: "100vh" }}>merge-test</h1>
     </div>
   );
 };
